c9b6b6970e45533db87b07d519b803c9da477f7e
Mikael <it-hogskolan@Mikaels-Macbook.local>
2015-01-22 14:23:15 +0100

    Initial Commit

---
+++ ColorMixer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ColorMixer
+//
+//  Created by IT-Högskolan on 2015-01-22.
+//  Copyright (c) 2015 IT-Högskolan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
