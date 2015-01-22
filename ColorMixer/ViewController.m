2a1c9e596b1fcdc1d1e9aa73b68dbe3acfb930dd
Mikael <it-hogskolan@Mikaels-Macbook.local>
2015-01-22 15:37:01 +0100

    en commit

--- ColorMixer/ViewController.m
+++ ColorMixer/ViewController.m
@@ -10,13 +10,43 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *colorDisplay;
+@property (weak, nonatomic) IBOutlet UISlider *redSlider;
+@property (weak, nonatomic) IBOutlet UISlider *greenSlider;
+@property (weak, nonatomic) IBOutlet UISlider *blueSlider;
+@property (weak, nonatomic) IBOutlet UILabel *redLabel;
+@property (weak, nonatomic) IBOutlet UILabel *greenLabel;
+@property (weak, nonatomic) IBOutlet UILabel *blueLabel;
+
 @end
 
 @implementation ViewController
 
+- (UIColor*) currentColor {
+    return [UIColor colorWithRed:self.redSlider.value
+                           green:self.greenSlider.value
+                            blue:self.blueSlider.value
+                           alpha:1.0f];
+}
+
+- (IBAction)sliderChanged:(id)sender {
+    self.colorDisplay.backgroundColor = [self currentColor];
+    
+    self.redLabel.text = [NSString stringWithFormat:@"%.2f", self.redSlider.value];
+
+    self.greenLabel.text = [NSString stringWithFormat:@"%.2f", self.greenSlider.value];
+
+    self.blueLabel.text = [NSString stringWithFormat:@"%.2f", self.blueSlider.value];
+
+}
+
+
+
+
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    [self sliderChanged:nil];
 }
 
 - (void)didReceiveMemoryWarning {
